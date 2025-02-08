2964eb9bf6234453b6146081efc052b1445cadf6
Lin2025-888 <s96024n@gmail.com>
2025-02-07 19:06:43 -0600

    Added two new files cli.py and functions.py with the docstrings.

---
+++ task_timer/cli.py
@@ -0,0 +1,9 @@
+"""
+cli.py
+Anna Chen
+
+Created: 2025-02-07
+Last Updated: 2025-02-07
+
+This file contains the command-line interface logic for the task timer program.
+"""
---
+++ task_timer/functions.py
@@ -0,0 +1,9 @@
+"""
+functions.py
+Anna Chen
+
+Created: 2025-02-07
+Last Updated: 2025-02-07
+
+This file contains the functions needed for the task timer program.
+"""
