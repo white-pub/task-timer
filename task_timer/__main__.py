765465493327bda60c90e4a86d8f3d0c1c24f5c6
Lin2025-888 <s96024n@gmail.com>
2025-02-07 21:33:39 -0600

    Moved the control logic from cli.py to __main__.py file for simplicity. Deleted cli.py

--- task_timer/__main__.py
+++ task_timer/__main__.py
@@ -1,11 +1,50 @@
 """
 __main__.py
 Anna Chen
-2025-01-30
+Created: 2025-01-30
+Last Updated: 2025-02-07
  
 This file is the starting point of the program.
+It also contains the command-line interface logic for the task timer program.
 """
 
- 
+# import the TaskManager class which has all the important methods
+from functions import TaskManager
+
+def main():
+
+    # Initialize the task dataframe
+    task_manager = TaskManager()
+
+    # Display program name and introduce available commands
+    print("\n-----------------------------------------------\n")
+    print("                Task Timer\n")
+    print("  This is a command line interface program that allow the user to ")
+    print("  track multiple tasks at a time.")
+    task_manager.list_commands()
+
+    while True:
+
+        # prompt user input and change inout to all lower case for consistancy
+        user_input = input("Enter command:")
+        user_input = user_input.lower()
+
+        # start new task
+        if user_input == "start task":
+            task_manager.start_task()
+
+        elif user_input == "help":
+            task_manager.list_commands()
+        
+        # exit program
+        elif user_input == "exit":
+            break
+        
+        # Handle invalid input
+        else:
+            print("Invalid command.")
+            task_manager.list_commands()
+
+
 if __name__ == '__main__':
     main()
